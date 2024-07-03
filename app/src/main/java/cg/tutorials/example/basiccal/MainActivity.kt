0069bcd97a74d534a4d0a3f0030bb4a68cd5b207
Chaitnya-Ghai <chaitnyaghai2004@gmail.com>
2024-07-03 13:28:14 +0530

    binding

--- app/src/main/java/cg/tutorials/example/basiccal/MainActivity.kt
+++ app/src/main/java/cg/tutorials/example/basiccal/MainActivity.kt
@@ -7,23 +7,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import cg.tutorials.example.basiccal.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    var ter :TextView?=null
-    var plus :Button?= null
-    var minus :Button?= null
-    var multiply :Button?=null
-    var divide :Button?= null
-
+//    var ter :TextView?=null
+//    var plus :Button?= null
+//    var minus :Button?= null
+//    var multiply :Button?=null
+//    var divide :Button?= null
+    lateinit var binding :ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ter = findViewById(R.id.btn1)
-        plus = findViewById(R.id.plus)
-        minus = findViewById(R.id.minus)
-        multiply= findViewById(R.id.multiply)
-        divide = findViewById(R.id.divide)
+        binding =ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        ter = findViewById(R.id.btn1)
+//        plus = findViewById(R.id.plus)
+//        minus = findViewById(R.id.minus)
+//        multiply= findViewById(R.id.multiply)
+//        divide = findViewById(R.id.divide)
 
 
 
@@ -32,17 +34,18 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        plus?.setOnClickListener {
-            ter?.text = "${ter?.text.toString().toInt() +2}"
+        binding.plus.setOnClickListener {
+//            binding.btn1.text = "${binding.btn1.text.toString().toInt() +2}"
+            binding.btn1.setText((binding.btn1.text.toString().toInt() +2).toString())
         }
-        minus?.setOnClickListener {
-            ter?.text = "${ter?.text.toString().toInt()-2}"
+        binding.minus.setOnClickListener {
+           binding.btn1.text = "${binding.btn1.text.toString().toInt()-2}"
         }
-        multiply?.setOnClickListener {
-            ter?.text = "${ter?.text.toString().toInt()*2}"
+        binding.multiply.setOnClickListener {
+            binding.btn1.text = "${binding.btn1.text.toString().toInt()*2}"
         }
-        divide?.setOnClickListener {
-            ter?.text = "${ter?.text.toString().toInt()/2}"
+        binding.divide.setOnClickListener {
+            binding.btn1.text = "${binding.btn1.text.toString().toInt()/2}"
         }
 
     }
